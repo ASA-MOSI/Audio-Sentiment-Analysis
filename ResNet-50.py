ca37ed3dbad6ae88851285fe193dad4fe7295487
Feiyang Chen <Eurus_Holmes_999@163.com>
2018-08-16 08:09:23 +0000

    Update ResNet-50.py

--- ResNet-50.py
+++ ResNet-50.py
@@ -14,45 +14,6 @@ from keras.optimizers import Adam
 seed = 1234
 np.random.seed(seed)
 
-# 写一个LossHistory类，保存loss和acc
-class LossHistory(keras.callbacks.Callback):
-    def on_train_begin(self, logs={}):
-        self.losses = {'batch': [], 'epoch': []}
-        self.accuracy = {'batch': [], 'epoch': []}
-        self.val_loss = {'batch': [], 'epoch': []}
-        self.val_acc = {'batch': [], 'epoch': []}
-
-    def on_batch_end(self, batch, logs={}):
-        self.losses['batch'].append(logs.get('loss'))
-        self.accuracy['batch'].append(logs.get('acc'))
-        self.val_loss['batch'].append(logs.get('val_loss'))
-        self.val_acc['batch'].append(logs.get('val_acc'))
-
-    def on_epoch_end(self, batch, logs={}):
-        self.losses['epoch'].append(logs.get('loss'))
-        self.accuracy['epoch'].append(logs.get('acc'))
-        self.val_loss['epoch'].append(logs.get('val_loss'))
-        self.val_acc['epoch'].append(logs.get('val_acc'))
-
-    def loss_plot(self, loss_type):
-        iters = range(len(self.losses[loss_type]))
-        plt.figure()
-        # acc
-        plt.plot(iters, self.accuracy[loss_type], 'r', label='train acc')
-        # loss
-        plt.plot(iters, self.losses[loss_type], 'g', label='train loss')
-        if loss_type == 'epoch':
-            # val_acc
-            plt.plot(iters, self.val_acc[loss_type], 'b', label='val acc')
-            # val_loss
-            plt.plot(iters, self.val_loss[loss_type], 'k', label='val loss')
-        plt.grid(True)
-        plt.xlabel(loss_type)
-        plt.ylabel('acc-loss')
-        plt.legend(loc="upper right")
-        plt.show()
-
-
 # Turn off TF verbose logging
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 print("------Begin------")
@@ -137,20 +98,15 @@ sgd = SGD(decay=0.0001, momentum=0.9)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 model.summary()
 
-
-# 创建一个实例history
-history = LossHistory()
 print("Training ...")
 model.fit(features.train_X, features.train_Y, batch_size=batch_size, verbose=1,
-          epochs=nb_epochs, validation_data=(features.test_X, features.test_Y), callbacks=[history], )
+          epochs=nb_epochs, validation_data=(features.test_X, features.test_Y))
 
 print("\nTesting ...")
 score, accuracy = model.evaluate(features.test_X, features.test_Y, batch_size=batch_size, verbose=1)
 print("Test loss:  ", score)
 print("Test accuracy:  ", accuracy)
 
-# 绘制acc-loss曲线
-history.loss_plot('epoch')
 
 
 
