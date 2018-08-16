b23dc56146aceb260558a4a5cde47bf7598b4511
Feiyang Chen <Eurus_Holmes_999@163.com>
2018-08-16 10:11:04 +0800

    update test

--- Extract_feature_2_class.py
+++ Extract_feature_2_class.py
@@ -1,38 +1,21 @@
-# -*- coding:utf-8 -*- 
-__Author__ = "Feiyang Chen"
-__Time__ = '2018/8/14'
-__Title__ = 'Audio_Sentiment_Analysis'
-
-"""
- code is far away from bugs with the god animal protecting
-    I love animals. God bless you.
-              ┏┓      ┏┓
-            ┏┛┻━━━┛┻┓
-            ┃      ☃      ┃
-            ┃  ┳┛  ┗┳  ┃
-            ┃      ┻      ┃
-            ┗━┓      ┏━┛
-                ┃      ┗━━━┓
-                ┃  神兽保佑    ┣┓
-                ┃ 永无BUG！   ┏┛
-                ┗┓┓┏━┳┓┏┛
-                  ┃┫┫  ┃┫┫
-                  ┗┻┛  ┗┻┛
-"""
-
 import numpy as np
 import librosa
 import math
 import re
+import os
 import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+
 
 
 class Extract_feature:
 
     hop_length = None
-    genre_list = ['neutral', 'polarity']
+    genre_list = ['negative', 'neutral', 'positive']
 
     dir_trainfolder = "./data/_train"
+
     dir_testfolder = "./data/_test"
     dir_all_files = "./data"
 
@@ -49,30 +32,32 @@ class Extract_feature:
     def __init__(self):
         self.hop_length = 512
         self.timeseries_length_list = []
-        # self.timeseries = []
-        # self.timeseriesindex = []
-        # self.timeseriesdict = dict(zip(self.timeseriesindex, self.timeseries))
+
+    def min_max_norm(self,x):
+        x_norm = (x - np.mean(x)) / np.std(x)
+        return x_norm
 
     def load_preprocess_data(self):
-
         self.trainfiles_list = self.path_to_audiofiles(self.dir_trainfolder)
+
         self.testfiles_list = self.path_to_audiofiles(self.dir_testfolder)
 
         all_files_list = []
-        # print("长度1是： %d" % len(all_files_list))
+        print("len1:%d"%len(all_files_list))
         all_files_list.extend(self.trainfiles_list)
-        # print("长度2是： %d" % len(all_files_list))
+        print("len2:%d"%len(all_files_list))
         print(all_files_list[0])
         print(all_files_list[1])
         print(all_files_list[2])
         all_files_list.extend(self.testfiles_list)
         print(all_files_list[2198])
 
-        self.precompute_min_timeseries_len(all_files_list)
+    #    self.precompute_min_timeseries_len(all_files_list)
+#        print(min(self.timeseries_length_list))
         print("[DEBUG] total number of files: " + str(len(self.timeseries_length_list)))
 
         # Training set
-        # print("????????????????????")
+
         print(self.trainfiles_list[0])
         print(self.trainfiles_list[1])
         print(self.trainfiles_list[2])
@@ -83,6 +68,7 @@ class Extract_feature:
         with open(self.train_Y_preprocessed_data, 'wb') as f:
             self.train_Y = self.one_hot(self.train_Y)
             np.save(f, self.train_Y)
+
 
         # Test set
         self.test_X, self.test_Y = self.extract_audio_features(self.testfiles_list)
@@ -102,19 +88,13 @@ class Extract_feature:
 
     def precompute_min_timeseries_len(self, list_of_audiofiles):
         for file in list_of_audiofiles:
-            # self.timeseriesindex.append(str(file))
             print("Loading " + str(file))
             y, sr = librosa.load(file)
             self.timeseries_length_list.append(math.ceil(len(y) / self.hop_length))
-            # self.timeseries.append(math.ceil(len(y) / self.hop_length))
-            # self.timeseriesdict = dict(zip(self.timeseriesindex, self.timeseries))
-            # print(self.timeseriesdict)
+
 
     def extract_audio_features(self, list_of_audiofiles):
-        timeseries_length_min = min(self.timeseries_length_list)
-        timeseries_length_max = max(self.timeseries_length_list)
-        print(timeseries_length_min)
-        print(timeseries_length_max)
+        #timeseries_length = min(self.timeseries_length_list)
         print(len(list_of_audiofiles))
         print("*******************")
         print(list_of_audiofiles[0])
@@ -122,71 +102,66 @@ class Extract_feature:
         print(list_of_audiofiles[2])
         print("*******************")
         print(list_of_audiofiles)
-
+        timeseries_length = 8
+        data = np.zeros((len(list_of_audiofiles), timeseries_length, 33), dtype=np.float64)
         target = []
-        target_ret = []
-        data_path = ""
-        # ./data/_train/dvsfs.wav
+        target_ret=[]
+        data_path=""
 
-        target = self.load_target(target)
-        print("+++++++++++")
-        print(len(target))
-        print("+++++++++++")
-        target_ret[:] = target[0:1616]
-        print(len(target_ret))
-        print(target)
-        print("+++++++++++")
-        a = 0
-        b = 0
+        a=0
+        b=0
+        c=0
         for l in target:
-            if l == "polarity":
-                a += 1
-            elif l == "neutral":
-                b += 1
-        print("polarity:%d, neutral:%d" % (a, b))
+            if l =="negative":
+                a+=1
+            elif l=="neutral":
+                b+=1
+            else:
+                c+=1
+        print("a:%d,b:%d,c:%d"%(a,b,c))
 
         for i, file in enumerate(list_of_audiofiles):
             print(i)
             print(file)
-            timeseries_length2 = 256
-            # print(timeseries_length2)
-            data = np.zeros((len(list_of_audiofiles), timeseries_length2, 33), dtype=np.float64)
+
             y, sr = librosa.load(file)
-
             mfcc = librosa.feature.mfcc(y=y, sr=sr, hop_length=self.hop_length, n_mfcc=13)
-            mfcc = self.Extended_array(mfcc)
-            # print(mfcc.shape)
-
             spectral_center = librosa.feature.spectral_centroid(y=y, sr=sr, hop_length=self.hop_length)
-            spectral_center = self.Extended_array(spectral_center)
-
             chroma = librosa.feature.chroma_stft(y=y, sr=sr, hop_length=self.hop_length)
-            chroma = self.Extended_array(chroma)
-
             spectral_contrast = librosa.feature.spectral_contrast(y=y, sr=sr, hop_length=self.hop_length)
-            spectral_contrast = self.Extended_array(spectral_contrast)
 
-            print(mfcc.T.shape)
+            print("++++++")
+            print(mfcc)
+            print(type(mfcc))
+            print(mfcc.shape)
 
-            data[i, :, 0:13] = mfcc.T[0:timeseries_length2, :]
-            data[i, :, 13:14] = spectral_center.T[0:timeseries_length2, :]
-            data[i, :, 14:26] = chroma.T[0:timeseries_length2, :]
-            data[i, :, 26:33] = spectral_contrast.T[0:timeseries_length2, :]
+            print("{{{{{}}}}}}}")
 
-            data_path = file
+            data[i, :, 0:13] = mfcc.T[0:timeseries_length, :]
+            data[i, :, 13:14] = spectral_center.T[0:timeseries_length, :]
+            data[i, :, 14:26] = chroma.T[0:timeseries_length, :]
+            data[i, :, 26:33] = spectral_contrast.T[0:timeseries_length, :]
+
+            data_path=file
 
             print("Extracted features audio track %i of %i." % (i + 1, len(list_of_audiofiles)))
 
-        print("data_path:%s " % data_path)
+
+        print("data_path:%s "%data_path)
         splits = re.split('[ .]', data_path)
         train_or_test = re.split('[ /]', splits[1])[2]
-        print("train_or_test:%s " % train_or_test)
+        print("train_or_test:%s "%train_or_test)
         target = self.load_target(target)
-        if train_or_test == "_train":
-            target_ret[:] = target[0:1616]
+        if train_or_test=="_train":
+            target_ret[:]=target[0:1616]
         else:
-            target_ret[:] = target[1616:2199]
+            target_ret[:]=target[1616:2199]
+
+        data=self.min_max_norm(data)
+
         return data, np.expand_dims(np.asarray(target_ret), axis=1)
+
+
 
     def one_hot(self, Y_genre_strings):
         y_one_hot = np.zeros((Y_genre_strings.shape[0], len(self.genre_list)))
@@ -197,38 +172,27 @@ class Extract_feature:
 
     def path_to_audiofiles(self, dir_folder):
         list_of_audio = []
-        if dir_folder == "./data/_train":
-            for i in range(1, 1617):
-                file_name = "%s%s%s%s" % (dir_folder, "/", str(i), ".wav")
+        if dir_folder=="./data/_train":
+            for i in range(1,1617):
+                file_name="%s%s%s%s"%(dir_folder,"/",str(i),".wav")
                 list_of_audio.append(file_name)
         else:
             for i in range(1617,2200):
-                file_name = "%s%s%s%s" % (dir_folder, "/", str(i), ".wav")
+                file_name ="%s%s%s%s"%(dir_folder,"/",str(i),".wav")
                 list_of_audio.append(file_name)
         return list_of_audio
+
 
     def load_target(self, target):
         all_label = pd.read_csv("./OpinionLevelSentiment.csv", header=None)
         arr = np.array(all_label)
         all_label_list = arr.tolist()
         for i in range(0, 2199):
-            if all_label_list[i][4] > -1 and all_label_list[i][4] <= 1:
-                target.append("neutral")
+            if all_label_list[i][4] > 1:
+                target.append("positive")
+            elif all_label_list[i][4] < -1:
+                target.append("negative")
             else:
-                target.append("polarity")
-        # print(len(target))
+                target.append("neutral")
+        print(len(target))
         return target
-
-    def Extended_array(self, feature):
-        if feature.shape[1] < 256:
-            # print(feature.shape)
-            feature_mean = np.mean(feature, axis=1)
-            feature_mean = feature_mean.reshape(len(feature_mean), -1)
-            # print(feature_mean.shape)
-            feature_extend = np.tile(feature_mean, 256-feature.shape[1])
-            # print(feature_extend.shape)
-            feature_extended = np.concatenate((feature, feature_extend), axis=1)
-            # print(feature_extended)
-            return feature_extended
-        else:
-            return feature
